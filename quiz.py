9d7228359e8b2faaab44eee5dc98411416cfa0d2
tim0867 <43320119+tim0867@users.noreply.github.com>
2023-06-04 10:55:10 +0100

    Added .lower for and score

--- quiz.py
+++ quiz.py
@@ -1,31 +1,38 @@
 print("Welcome to my Computer Quiz!!")
 
 playing = input("Would you like to play?\n")
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Great, let's crack on!\n")
+score = 0
 
 answer = input("What does CPU stand for?\n")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct, Well Done!\n")
+    score += 1
 else:
     print("Sorry, that's wrong\n")
 
 answer = input("What does GPU stand for?\n")
-if answer == "graphical processing unit":
+if answer.lower() == "graphical processing unit":
     print("Correct, Well Done!\n")
+    score += 1
 else:
     print("Sorry, that's wrong\n")
 
 answer = input("What does RAM stand for?\n")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print("Correct, Well Done!\n")
+    score += 1
 else:
     print("Sorry, that's wrong\n")
 
 answer = input("What does PSU stand for?\n")
-if answer == "power supply unit":
+if answer.lower() == "power supply unit":
     print("Correct, Well Done!\n")
+    score += 1
 else:
     print("Sorry, that's wrong\n")
+
+print ("Well done you have scored " + str(score) + " points\n\n")
